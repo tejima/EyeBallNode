cae0e259a87da9dff4b3840c2ae7c04a61cacbc4
Takeshi Namikawa <takeshi.namikawa@gmail.com>
2012-11-25 00:15:38 +0900

    演出修正

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -27,19 +27,19 @@ var themes = {
 		messageColor : '#000000'
 	},
 	committed : {
-		mp3 : 'machinegun.mp3',
-		bg : 'putin_see.jpg',
+		mp3 : 'bell.mp3',
+		bg : 'putin_angry.jpg',
 		messageY : '80%',
 		messageColor : '#ffffff'
 	},
 	failed : {
-		mp3 : 'failed.mp3',
-		bg : 'putin_failed2.jpg',
+		mp3 : 'machinegun.mp3',
+		bg : 'putin_machinegun.jpg',
 		messageY : '5%',
 		messageColor : '#ffffff'
 	},
 	succeed : {
-		mp3 : 'succeed.mp3',
+		mp3 : 'cheers.mp3',
 		bg : 'putin_succeed.jpg',
 		messageY : '70%',
 		messageColor : '#000000'
@@ -47,12 +47,29 @@ var themes = {
 
 };
 
-var defaultInput = {
-	theme : 'waiting',
-	message : 'ほげほげさんがコミットしました。<br />テストは<span style="color:red">失敗</span>しました。'
+var currentDatetime = function() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth() + 1;
+	var week = now.getDay();
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+
+	var weekdays = new Array("日", "月", "火", "水", "木", "金", "土");
+
+	return month + "<span style=\"font-size:80%\">/</span>" + day
+			+ "<span style=\"font-size:80%\">（" + weekdays[week] + "）</span> "
+			+ ('0' + hour).substr(-2)
+			+ '<span style=\"font-size:80%\">:</span>' + ('0' + min).substr(-2);
 };
 
-var serverUrl = 'http://eyeball.herokuapp.com/index.php';
+var defaultInput = {
+	theme : 'waiting',
+	message : currentDatetime()
+};
+
+var serverUrl = '/polling.json';
 
 var checkServer = function() {
 	$.getJSON(serverUrl, null, function(data, status) {
@@ -66,6 +83,3 @@ var checkServer = function() {
 
 // 10秒ポーリング、更新があればshowMessage
 setInterval("checkServer()", 10000);
-
-
-
