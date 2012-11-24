4e9000468bc3c82183cefc5902e2341a4da15465
手嶋守 <tejima@gmail.com>
2012-11-24 23:40:58 +0900

    split method

--- web.js
+++ web.js
@@ -8,27 +8,29 @@ var port = process.env.PORT || 5000;
 
 app.post('/hook.json',function(req,res){
   console.log('request =' + JSON.stringify(req.body.payload));
-  var target = req.body.target;
   var json = JSON.parse(req.body.payload);
+
   var message = "";
-  switch(target){
-  	case "github":
-	  var project_name = json.repository.name;
-  	  var commiter_name = json.commits[0].author.name;
-  	  var message = commiter_name + "さんがコミットしました。" + project_name;
-  	  break;
-  	case "travis":
-  	  var project_name = json.repository.name;
-  	  var commiter_name = json.committer_name;
-  	  var status_message = json.status_message;
-  	  var message = commiter_name + "さんが" + project_name + "のテストにトライし" + status_message + "しました";
-  	  break;
-    default:
-	  console.log("ERROR");
-      res.send("ERROR");
-  }
+  var project_name = json.repository.name;
+  var commiter_name = json.commits[0].author.name;
+  var message = commiter_name + "さんがコミットしました。" + project_name;
   console.log(message);
   res.send(message);
 });
+
+app.post('/hook_travis.json',function(req,res){
+  console.log('request =' + JSON.stringify(req.body.payload));
+  var json = JSON.parse(req.body.payload);
+
+  var message = "";
+  var project_name = json.repository.name;
+  var commiter_name = json.committer_name;
+  var status_message = json.status_message;
+  var message = commiter_name + "さんが" + project_name + "のテストにトライし" + status_message + "しました";
+  console.log(message);
+  res.send(message);
+});
+
+
 
 app.listen(port);
