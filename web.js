7af7d95de76bef03cacb1647e4a8598484735e01
手嶋守 <tejima@gmail.com>
2012-11-24 23:09:48 +0900

    hook.json

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.bodyParser());
 
 var port = process.env.PORT || 5000;
 
-app.post('/hook.php',function(req,res){
+app.post('/hook.json',function(req,res){
   console.log('request =' + JSON.stringify(req.body.payload));
   var target = req.body.target;
   var json = JSON.parse(req.body.payload);
