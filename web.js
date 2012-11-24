784c799981ef66c8d1ac285a1762ed4f52e7fecd
手嶋守 <tejima@gmail.com>
2012-11-24 21:10:41 +0900

    use connect as standard webserver

--- web.js
+++ web.js
@@ -1,18 +1,9 @@
 var express = require('express');
+var connect = require('connect');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger()).listen(5000);
+connect.createServer(connect.static(__dirname+"/public_html")).listen(8080);
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname + '/public_html')
-).listen(8080);
