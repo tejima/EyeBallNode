96c8c521d8aff8fb2344c309738c3c10c87e179d
手嶋守 <tejima@gmail.com>
2012-11-27 21:54:06 +0900

    マニュアル投稿対応

--- web.js
+++ web.js
@@ -18,6 +18,24 @@ app.get('/polling.json',function(req,res){
   }
 
 });
+ 
+app.get('/hook_manual.json',function(req,res){
+  var deviceid = parseInt(req.query.deviceid) ? parseInt(req.query.deviceid) : 0;
+  var message = req.query.message ? req.query.message : null;
+  var theme = req.query.theme ? req.query.theme : null;
+  var status = req.query.status ? req.query.status : null;
+  if(message && theme && status){
+    if(stackPush(deviceid,message,theme,status)){
+      res.send(JSON.stringify({"status":"SUCCESS"}));    
+    }else{
+    　　res.send(JSON.stringify({"status":"PARAM_ERROR"}));
+    }
+    res.send(JSON.stringify(result));
+  }else{
+    console.log(req.body);
+    res.send(JSON.stringify({"status":"PRAM_ERROR"}));
+  }
+});
 
 app.post('/hook.json',function(req,res){
   var deviceid = parseInt(req.body.deviceid) ? parseInt(req.body.deviceid) : 0;
